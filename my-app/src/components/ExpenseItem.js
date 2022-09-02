9bd600fd3a46377bb74da5d4a501e05fe6cee048
minsoll <sol020501@hufs.ac.kr>
2022-09-02 23:08:34 +0900

    ExpenseItem component

---
+++ my-app/src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+// 비용아이템
+function ExpenseItem() {
+  return ( <div>
+    <div>Date</div>
+    <div>
+      <h2>Title</h2>
+      <div>Amount</div>
+    </div>
+  </div>
+ );
+}
+
+export default ExpenseItem;
