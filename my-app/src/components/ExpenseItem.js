37869ec8d77fb3f1dffc11b1f6b143be399c9386
minsoll <sol020501@hufs.ac.kr>
2022-09-04 22:51:02 +0900

    ExpenseDate

--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -1,18 +1,14 @@
 // 비용아이템
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 9, 3);
-  const expenseTitle = 'car Insurance';
-  const expenseAmount = 294.67;
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
-      {/* Date는 객체이기 때문에  <div>{expenseDate}</div>처럼 문자로 출력할 수 없다. */}
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
  );
